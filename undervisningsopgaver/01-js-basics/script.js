7597777faa09083352861b751b40429259c2e912
sylvesterflaaten <sylv1791@stud.kea.dk>
2020-02-03 11:46:13 +0100

    opgave 3 og 4 løst

--- undervisningsopgaver/01-js-basics/script.js
+++ undervisningsopgaver/01-js-basics/script.js
@@ -1,14 +1,18 @@
-let længde = 3;
-let bredde = 5;
-let areal = længde * bredde;
+/*let længde = 3;
+let bredde = 5;*/
+
 
 let resultat;
 
+let name;
 
-/*
+
+/* Opgave 1
 let resultat = `Længden er ${længde} meter og bredden er ${bredde} meter. Arealet er ${areal}`;
 console.log(resultat);*/
 
+/*Opgave 2 er arealet mellem 100 og 200?*/
+/*
 
 if (areal < 100) {
     resultat = "Arealet er for lille";
@@ -23,3 +27,31 @@ if (areal >= 200) {
 }
 
 console.log(resultat);
+*/
+
+
+/*Opgave 3 sig goddag*/
+
+/*document.querySelector(".videre").addEventListener("click", videreKnap);
+
+function videreKnap() {
+
+    let name = document.querySelector("#name").value
+    console.log(`Hej ${name}`);
+}*/
+
+/*Opgave 4 */
+/*
+
+document.querySelector(".resultat").addEventListener("click", resultatKnap);
+
+function resultatKnap() {
+
+    let længde = document.querySelector("#laengde").value;
+    let bredde = document.querySelector("#bredde").value;
+
+    let areal = længde * bredde;
+
+    console.log(`Arealet er ${areal}`);
+}
+*/
