1cd86eb1d1d7d57922144d50254318fd1a72e7d6
sylvesterflaaten <sylv1791@stud.kea.dk>
2020-02-03 10:50:14 +0100

    areal 02 løst

--- undervisningsopgaver/01-js-basics/script.js
+++ undervisningsopgaver/01-js-basics/script.js
@@ -1,7 +1,25 @@
 let længde = 3;
 let bredde = 5;
-
 let areal = længde * bredde;
 
+let resultat;
+
+
+/*
 let resultat = `Længden er ${længde} meter og bredden er ${bredde} meter. Arealet er ${areal}`;
+console.log(resultat);*/
+
+
+if (areal < 100) {
+    resultat = "Arealet er for lille";
+}
+
+if (areal >= 100 && areal < 200) {
+    resultat = "Arealet er okay";
+}
+
+if (areal >= 200) {
+    resultat = "Arealet er for stort";
+}
+
 console.log(resultat);
